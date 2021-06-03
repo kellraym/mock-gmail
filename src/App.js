144dd79b1d163d8cd50f256f53b57b596deffb90
kellraym <ray.al.kell@gmail.com>
2021-06-03 16:00:09 -0600

    Add functionality for send email

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     this.getEmail = this.getEmail.bind(this);
     this.open = this.open.bind(this);
     this.close = this.close.bind(this);
+    this.post = this.post.bind(this);
   }
 
   async componentDidMount() {
@@ -35,14 +36,37 @@ class App extends React.Component {
   getEmail(event) {
     let index = event.target.id
     let email = this.state.emails[index]
-    this.setState({currentEmail: email})
+    this.setState({ currentEmail: email })
     this.open()
   }
+
+  async post() {
+
+    fetch("http://localhost:3001/send", {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        sender: document.querySelector(`.sender`).value,
+        recipient: document.querySelector(`.recipient`).value,
+        subject: document.querySelector(`.subject`).value,
+        message: document.querySelector(`.message`).value,
+        date: document.querySelector(`.date`).value,
+        id: this.state.emails.length,
+      })
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+    this.componentDidMount();
+  }
+
   // href={`http://localhost:3001/emails/${ind + 1}`}
 
-  open() {
+  open(event) {
+    console.log(event);
     document.getElementById("modal").classList.remove(`hidden`);
-
+    // document.getElementById("sendModal").classList.remove(`hidden`);
   }
 
   close() {
@@ -56,6 +80,8 @@ class App extends React.Component {
       <div>
         <h1>it works</h1>
         <ol>{this.displayEmail()}</ol>
+
+
         <div id="modal" className="modal hidden">
           <div className="modal-content">
             <span className="close" onClick={this.close}>&times;</span>
@@ -67,7 +93,23 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+        
+        <input type="button" onClick={this.open} value="Compose email"/>
+        <div id="sendModal" className="sendModal hidden">
+          <div className="modal-content">
+            <span className="close" onClick={this.close}>&times;</span>
+            <div className="modalEmail"></div>
+              <form>
+                <input className="sender" type="text" placeholder="sender" />
+                <input className="recipient" type="text" placeholder="recipient" />
+                <input className="subject" type="text" placeholder="subject" />
+                <input className="message" type="text" placeholder="message" />
+                <input className="date" type="text" placeholder="date" />
+                <input type="button" onClick={this.post} value="send" />
+              </form>
+            </div>
+          </div>
+        </div>
     )
 
   }
